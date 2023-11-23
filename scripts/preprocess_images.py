58cc07606efd8034a14d3b39f99168d3c67bbdd2
“23jmo” <2023johnathanmo@gmail.com>
2023-11-22 22:40:37 -0500

    new download data script

--- scripts/preprocess_images.py
+++ scripts/preprocess_images.py
@@ -21,16 +21,20 @@ def preprocess_masks(image_directory):
     for image_path in tqdm(image_paths):
         convert_png_to_binary_gif(image_path)
 
-mask_directory = "/data/masks/"
-img_directory = "data/imgs/"
-test_mask_directory = "../data/test_masks/"
-test_img_directory = "data/test_imgs/"
+mask_directory = "data/masks/"
+img_directory = "data/images/"
+test_mask_directory = "data/test_masks/"
+test_img_directory = "data/test_images/"
 
 # print(glob.glob(img_directory + "*.png"))
 
-print(glob.glob("data/test_images/*.png"))
+preprocess_masks("data/test_masks/*.png")
+preprocess_masks("data/masks/*.png")
+
+for image in glob.glob("data/images/*.png"):
+    os.rename(image, image.replace("_img", ""))
+    
 for image in glob.glob("data/test_images/*.png"):
-    print(image)
     os.rename(image, image.replace("_img", ""))
 
 # preprocess_masks("data/test_masks/*.png")
